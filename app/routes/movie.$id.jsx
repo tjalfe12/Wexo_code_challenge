c7b16f91bcc0be8dd4710b374823fa9b4806e73c
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2024-05-07 15:00:49 +0200

    added more info to single movie page

--- app/routes/movie.$id.jsx
+++ app/routes/movie.$id.jsx
@@ -7,11 +7,20 @@ export let loader = async ({ params }) => {
     `https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas/${params.id}?form=json`
   );
   let data = await response.json();
-  return data;
+
+  const actors = data["plprogram$credits"]
+    .filter((credit) => credit["plprogram$creditType"] === "actor")
+    .map((actor) => actor["plprogram$personName"]);
+  const directors = data["plprogram$credits"]
+    .filter((credit) => credit["plprogram$creditType"] === "director")
+    .map((director) => director["plprogram$personName"]);
+  const releaseYear = data["plprogram$year"];
+
+  return { movie: data, actors, directors, releaseYear };
 };
 
 export default function Single_Movie_ID() {
-  let movie = useLoaderData();
+  const { movie, actors, directors, releaseYear } = useLoaderData();
 
   if (!movie.id) {
     return <div>Movie not found</div>;
@@ -25,7 +34,7 @@ export default function Single_Movie_ID() {
     <div className="py-2 max-w-full md:w-3/4 m-auto">
       <div className="flex flex-col items-center">
         <p className="text-2xl font-dosis tracking-widest mb-4 uppercase">
-          {movie.title}
+          {movie.title} ({releaseYear})
         </p>
         <MovieImage
           movieId={movie.id}
@@ -33,16 +42,22 @@ export default function Single_Movie_ID() {
             "max-h-60 sm:max-h-72 min-h-12 w-48 sm:w-72 object-cover rounded-md shadow-md transition-all duration-300 ease-in-out hover:shadow-lg"
           }
         />
-
         <div className="flex flex-col items-center gap-4 mt-4">
           <p>{movie.description}</p>
           <p className="text-rose-500 text-md font-dosis tracking-wider">
             {genres.join(" - ")}
           </p>
-
+          <div>
+            <span className="text-md font-semibold">Actors:</span>{" "}
+            <span className="text-sm">{actors.join(", ")}</span>
+          </div>
+          <div>
+            <span className="text-md font-semibold">Director(s):</span>{" "}
+            <span className="text-sm">{directors.join(", ")}</span>
+          </div>
           <WishlistButton movie_id={movie.id} />
         </div>
-        <hr className="w-1/2 border-1 border-primary my-2" />
+        <hr className="w-1/2 border-1 border-rose-500 my-16" />
       </div>
     </div>
   );
